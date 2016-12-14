bfde324554272bf58836f80e1fc0cbee57660068
Jonny Moore <jonnymoore121212@gmail.com>
2016-12-14 16:20:59 +0000

    01-animation works for buttons, animation ends nicely

--- 01 - JavaScript Drum Kit/src/DrumKit.js
+++ 01 - JavaScript Drum Kit/src/DrumKit.js
@@ -1,10 +1,27 @@
 document.onkeydown = function(event) {
   audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   if (!audio) return;
-  // "Rewind" audio file for quick repeated hits of same button
+  // "Rewind" audio file for quick repeated hits of same button:
   audio.currentTime = 0;
   audio.play();
+  animateButton(event);
 }
+
+animateButton = function(event) {
+  button = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  $(button).addClass('playing');
+  buttons = document.querySelectorAll('.key');
+  buttons.forEach(function(){
+    // Listen in for when the transition on the button ends (see CSS)
+    // then execute our method endAnimation:
+    button.addEventListener('transitionend', endAnimation);
+  });
+}
+
+endAnimation = function() {
+  $(this).removeClass('playing');
+}
+
 
 
 
