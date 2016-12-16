1d95fe1c0a5ba3b58037627d77650887af152a11
Jonny Moore <jonnymoore121212@gmail.com>
2016-12-16 17:56:11 +0000

    02-logic calculates rotation for each clock hand, no update every sec yet

---
+++ 02 - JS + CSS Clock/src/Clock.js
@@ -0,0 +1,13 @@
+var d = new Date();
+var hours = d.getHours() % 12;
+var mins = d.getMinutes();
+var secs = d.getSeconds();
+
+const degreesInHour = 30;
+const degreesInMinute = 6;
+const degreesInSecond = 6;
+
+// +90 degrees since hands in the 12 o-clock position are ALREADY rotated 90 degrees:
+document.getElementsByClassName('hour-hand')[0].style.transform = `rotate(${hours * degreesInHour + 90}deg)`;
+document.getElementsByClassName('min-hand')[0].style.transform = `rotate(${mins * degreesInMinute + 90}deg)`;
+document.getElementsByClassName('second-hand')[0].style.transform = `rotate(${secs * degreesInSecond + 90}deg)`;
