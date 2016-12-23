5790b06e3d1aaf31b45cee4a4722b66900f87e50
Jonny Moore <jonnymoore121212@gmail.com>
2016-12-23 17:42:28 +0000

    05 - toggles active class AFTER relevant transitionend

--- 05 - Flex Panel Gallery/src/Flex.js
+++ 05 - Flex Panel Gallery/src/Flex.js
@@ -1,6 +1,14 @@
 panels = document.querySelectorAll('.panel');
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
 
 function toggleOpen() {
   $(this).toggleClass("open");
+}
+
+function toggleActive(event) {
+  if (event.propertyName.includes("flex")) {
+    $(this).toggleClass("active");
+  }
 }
